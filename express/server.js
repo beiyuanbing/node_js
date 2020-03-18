473df73a1fc8bb6a64ccf43a7112d2887b6b01a8
beiyuanbing <425210220@qq.com>
2020-03-18 10:13:41 +0800

    文件上传

--- express/server.js
+++ express/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+var formidable = require('formidable');
+var multiparty = require('multiparty');
 var User=require("./controller/user");
 app.use('/public', express.static('public'));
  
@@ -18,6 +21,34 @@ app.get('/process_get', function (req, res) {
    user.setUrl(req.query.last_name);
    user.add();
    res.end(JSON.stringify(response));
+});
+//文件上传相关
+app.post('/file',function(req, res) {
+   let form = new multiparty.Form();
+  /* 设置编辑 */
+  form.encoding = 'utf-8';
+  //设置文件存储路劲
+  form.uploadDir = './tmplFile';
+  form.parse(req, function (err, fields, files) {
+   try {
+     const content=fields.content;
+     let inputFile = files.file[0];
+     console.log(content);
+     console.log(inputFile.originalFilename);
+     let uploadedPath = inputFile.path;
+     let newPath = form.uploadDir + "/" + inputFile.originalFilename;
+     //同步重命名文件名 fs.renameSync(oldPath, newPath)
+     fs.renameSync(inputFile.path, newPath);
+     res.send({ data: "上传成功！" });
+     //读取数据后 删除文件
+     // fs.unlink(newPath, function () {
+     //   console.log("删除上传文件");
+     // })
+   } catch (err) {
+     console.log(err);
+     res.send({ err: "上传失败！" });
+   };
+ });
 })
 
 var server = app.listen(8084, function () {
